c36cc7c07a205e4f03228670c0af1fae24ac4337
Joshua Peregrin <johnjoshuaperegrin2@gmail.com>
2025-10-30 09:22:31 +0800

    Initial Commit

---
+++ shell.nix
@@ -0,0 +1,23 @@
+let
+  nixpkgs = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/nixos-25.05.tar.gz";
+  };
+  pkgs = import nixpkgs { };
+in
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    nodejs_22
+    pkgs.bashInteractive
+  ];
+
+  # Ensure Nix uses this bash, not system bash
+  shell = pkgs.bashInteractive;
+
+  shellHook = ''
+    export PS1="\[\033[0;32m\][\u@\h \W]\\$ \[\033[0m\]"
+    echo -ne "\033[0;31mThesis Nix Environment\033[0m\n"
+    echo -ne "\033[0;31mNode Version:\033[0;33m $(node --version)\033[0m\n"
+    #node --version
+    
+  '';
+}
