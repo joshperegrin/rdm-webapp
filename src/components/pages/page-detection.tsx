02b77a0fd662844eb9380944304bed7c5d31163c
Joshua Peregrin <johnjoshuaperegrin2@gmail.com>
2026-01-17 11:56:52 +0800

    Added Button Component

--- src/components/pages/page-detection.tsx
+++ src/components/pages/page-detection.tsx
@@ -1,10 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { CirclePlay, CircleStop } from "lucide-react";
 
 function DetectionPage(){
+  const lmao = () => {}
   return (
     <div className="flex flex-row h-full w-full">
-      <div className="hidden md:flex md:w-1/3 lg:w-90 "></div>
+      <div className="hidden md:flex md:w-1/3 lg:w-90 ">
+        <Button onClick={lmao} className="cursor-pointer" variant="outline" size="sm">
+          <CirclePlay /> Start
+          <CircleStop /> Stop
+        </Button>
+      </div>
       <div className="flex flex-1">
-        
       </div>
     </div>
   )
