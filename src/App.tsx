8cccaa648f3497aa396afd26d019c0c4b448070a
Joshua Peregrin <johnjoshuaperegrin2@gmail.com>
2026-04-23 18:04:27 +0800

    revisions for defense

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react'
 import '@/App.css'
 import Sidebar from "@/components/sidebar/sidebar" 
-import { Routes, Route } from 'react-router'
+import { Navigate, Route, Routes } from 'react-router'
 import DetectionPage from './components/pages/page-detection'
 import MapPage from './components/pages/page-map'
 import ReportsPage from './components/pages/page-reports'
@@ -16,7 +16,7 @@ function App() {
         <Sidebar/>
         <main className="h-full w-full">
           <Routes>
-            <Route path="/" />
+            <Route path="/" element={<Navigate to="/map" replace />} />
             <Route path="/detection" element={<DetectionPage/>} />
             <Route path="/map"       element={<MapPage/>} />
             <Route path="/reports"   element={<ReportsPage/>} />
